0e9a34b18fae6041cc73c1981e026d7f539b55d4
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2025-02-05 13:02:05 +0100

    Remove cluster-formation loop test

--- cluster-formation/check.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-for cluster in $(ls -d output/*/*/); do
-    cat $cluster/*.nodes | sort | uniq | wc -l | grep -q 1 || echo "Nodes disagree! Iteration $i, cluster $cluster failed"
-done
-
